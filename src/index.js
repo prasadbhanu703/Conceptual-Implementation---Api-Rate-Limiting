17e4f8248ff1f25152e48660eb44fed1ec99cdb9
prasadbhanu703 <prasadbhanu703@gmail.com>
2020-12-11 13:19:38 +0530

    Api-rate-limit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
+const data = require('./initialData');
 const port = 3000
 app.use(express.urlencoded());
+
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
@@ -12,7 +14,30 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+const defaultData = [];
+for(let i=0; i<10; i++) {
+    defaultData[i] = data[i];
+}
+app.get("/api/posts" , (req, res) => {
 
+    res.send(defaultData);
+});
+
+app.get("/api/posts/:id", (req,res) => {
+    const length = req.params.id;
+    if(length > 20) {
+
+    res.send(defaultData);
+    } 
+    else {
+        const newData = [];
+    for(let i=0; i<length; i++) {
+        newData[i] = data[i];
+    }
+
+    res.send(newData);
+    }
+}) 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
